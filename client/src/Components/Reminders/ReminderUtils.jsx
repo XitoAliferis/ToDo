b703ce9914c1e2e5b68821f64080131226237410
Xristopher Aliferis <xaliferis@1valet.com>
2024-07-24 14:20:17 -0400

    Changed {timezone again}

--- client/src/Components/Reminders/ReminderUtils.jsx
+++ client/src/Components/Reminders/ReminderUtils.jsx
@@ -2,5 +2,5 @@ import moment from 'moment-timezone';
 
 export const convertToLocal = (utcDate) => {
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone; // Get the user's timezone
-  return moment.utc(utcDate).tz(timezone).format('YYYY-MM-DDTHH:mm');
+  return moment.utc(utcDate).add(4, 'hours').tz(timezone).format('YYYY-MM-DDTHH:mm');
 };
